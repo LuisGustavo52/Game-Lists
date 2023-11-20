2ef45b655ef7b13cf50d1b834f7b7b6722516528
Luis Gustavo Teodoro da Silva <luisgustavo.teodoro52@gmail.com>
2023-11-20 18:13:37 -0300

    Mudei o retorno da classe GameController para o tipo GameMinDTO

--- src/main/java/com/luisgustavo/GameList/services/GameService.java
+++ src/main/java/com/luisgustavo/GameList/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luisgustavo.gameList.dto.GameMinDTO;
 import com.luisgustavo.gameList.entities.Game;
 //import com.luisgustavo.gameList.dto.GameMinDTO;
 import com.luisgustavo.gameList.repositories.GameRepository;
@@ -15,8 +16,10 @@ public class GameService {
     @Autowired //injeta uma instancia do gameRepository na classe, no nível do framework
     private GameRepository gameRepository;
 
-    public List<Game> findAll(){
+    public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
-        return result;
+        List <GameMinDTO> dto = result.stream().map( x -> new GameMinDTO(x)).toList();
+        // utiliza o construtor de GameMinDTO e o método map para transformar cada Objeto Game x em um Objeto GameMinDTO, a partir da lista de games result
+        return dto;
     }
 }
