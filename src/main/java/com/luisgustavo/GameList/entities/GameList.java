ba0210a7fef3b9240af737e3b24593088d285a05
Luis Gustavo Teodoro da Silva <luisgustavo.teodoro52@gmail.com>
2024-01-28 16:05:56 -0300

    criaçãom da classe GameList

---
+++ src/main/java/com/luisgustavo/GameList/entities/GameList.java
@@ -0,0 +1,58 @@
+package com.luisgustavo.gameList.entities;
+
+public class GameList {
+    private Long Id;
+    private String name;
+
+    public GameList(){
+
+    }
+
+    public GameList(Long id, String name) {
+        Id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GameList other = (GameList) obj;
+        if (Id == null) {
+            if (other.Id != null)
+                return false;
+        } else if (!Id.equals(other.Id))
+            return false;
+        return true;
+    }
+    
+    
+}
